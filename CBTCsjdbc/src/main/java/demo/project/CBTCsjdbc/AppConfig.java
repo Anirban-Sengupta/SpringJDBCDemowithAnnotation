721e3d5da4d9b977c55898fbb9b4804089180fb4
Anirban sengupta <sengupta.ani.80@gmail.com>
2019-08-23 18:04:13 +0530

    Bean Configure Class

---
+++ CBTCsjdbc/src/main/java/demo/project/CBTCsjdbc/AppConfig.java
@@ -0,0 +1,35 @@
+package demo.project.CBTCsjdbc;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+@ComponentScan("demo.project.CBTCsjdbc")
+@PropertySource("classpath:database.properties")
+public class AppConfig {
+ 
+	@Autowired
+    Environment environment;
+	
+	private final String DRIVER = "driver";
+	private final String URL = "url";
+	private final String USER = "usern";
+	private final String PASSWORD = "password";
+	
+	@Bean
+	DataSource dataSource() {
+		DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+		driverManagerDataSource.setDriverClassName(environment.getProperty(DRIVER));
+		driverManagerDataSource.setUrl(environment.getProperty(URL));
+		driverManagerDataSource.setUsername(environment.getProperty(USER));
+		driverManagerDataSource.setPassword(environment.getProperty(PASSWORD));
+		return driverManagerDataSource;
+	}
+}
