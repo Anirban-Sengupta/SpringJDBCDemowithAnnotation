6e410c5931f2cda7acde6098c7c92623dcfe4d46
Anirban sengupta <sengupta.ani.80@gmail.com>
2019-08-23 18:06:07 +0530

    Main Method

---
+++ CBTCsjdbc/src/main/java/demo/project/CBTCsjdbc/App.java
@@ -0,0 +1,47 @@
+package demo.project.CBTCsjdbc;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	AnnotationConfigApplicationContext conxt = new AnnotationConfigApplicationContext(AppConfig.class);
+    	TrainDAO trainDAO = conxt.getBean(TrainDAO.class);
+    	
+    	System.out.println("Listing All Trains");
+    	
+    	for (Train t : trainDAO.getAllTrain()) {
+    		System.out.println(t);
+    	}
+       
+    	System.out.println("/nGetting Train");
+    	Train t1 = trainDAO.getTrainById(2);
+    	System.out.println(t1);
+    	
+    	System.out.println("/nCreating Train");
+    	Train t2 = new Train(3, 169.5f, "Local");
+    	System.out.println(t2);
+    	trainDAO.getInsert(t2);
+    	for (Train t : trainDAO.getAllTrain()) {
+    		System.out.println(t);
+    	}
+    	
+    	System.out.println("/nUpdating Taing");
+    	Train t3 = trainDAO.getTrainById(3);
+    	t3.setLength(200);
+    	t3.setType("Express");
+    	trainDAO.getUpdate(t3);
+    	for (Train t : trainDAO.getAllTrain()) {
+    		System.out.println(t);
+    	}
+    	 System.out.println("/nDeleting Train");
+    	 Train t4 = trainDAO.getTrainById(3);
+    	 trainDAO.getDelete(t4);
+    	 for (Train t : trainDAO.getAllTrain()) {
+     		System.out.println(t);
+     	}
+    	     conxt.close();
+    }
+    
+}
